dfb8b83322d1a6f16baa7e623f1c8312f292a47c
CHORI-SEC <imsanny2008@gmail.com>
2026-01-28 11:32:53 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,13 @@ function login() {
   const loading = document.getElementById("loading");
   if (loading) loading.style.display = "block";
 
+  const users = [
+  { user: "admin", pass: "1234", role: "admin" },
+  { user: "almacen", pass: "1234", role: "almacen" }
+];
+
   setTimeout(() => {
     window.location.href = "dashboard.html";
   }, 1000);
 }
+
